2a2a7e644401dfd086051350d84d977095b4fd59
Richard Alam <ritzalam@gmail.com>
2013-02-23 19:46:24 +0000

    - start cleanup of FS ESL handler

--- bigbluebutton-apps/src/main/java/org/bigbluebutton/live/IVoiceGateway.java
+++ bigbluebutton-apps/src/main/java/org/bigbluebutton/live/IVoiceGateway.java
@@ -1,5 +1,9 @@
 package org.bigbluebutton.live;
 
 public interface IVoiceGateway {
-
+	void getVoiceUsers(String voiceMeetingID);
+	void mute(String voiceMeetingID, int voiceUserID, boolean mute);	
+	void eject(String voiceMeetingID, int voiceUserID);
+	void record(String voiceMeetingID, String meetingID);
+	void broadcast(String voiceMeetingID, String meetingID);
 }
--- bigbluebutton-apps/src/main/java/org/bigbluebutton/webconference/voice/freeswitch/FreeswitchServiceProvider.java
+++ bigbluebutton-apps/src/main/java/org/bigbluebutton/webconference/voice/freeswitch/FreeswitchServiceProvider.java
@@ -18,87 +18,82 @@
 */
 package org.bigbluebutton.webconference.voice.freeswitch;
 
-import java.net.ConnectException;
-
-import org.freeswitch.esl.client.inbound.Client;
-import org.freeswitch.esl.client.inbound.InboundConnectionFailure;
-import org.freeswitch.esl.client.manager.ManagerConnection;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 import org.bigbluebutton.webconference.voice.ConferenceServiceProvider;
-import org.bigbluebutton.webconference.voice.events.ConferenceEventListener;
+import org.bigbluebutton.webconference.voice.commands.EjectVoiceUser;
+import org.bigbluebutton.webconference.voice.commands.GetVoiceUsers;
+import org.bigbluebutton.webconference.voice.commands.IVoiceCommand;
+import org.bigbluebutton.webconference.voice.commands.MuteVoiceUser;
 import org.red5.logging.Red5LoggerFactory;
 import org.slf4j.Logger;
 
-public class FreeswitchServiceProvider implements ConferenceServiceProvider {
+public class FreeswitchServiceProvider {
 	private static Logger log = Red5LoggerFactory.getLogger(FreeswitchServiceProvider.class, "bigbluebutton");
 	
-	private ConferenceServiceProvider appDelegate;
-	private ManagerConnection connection;
-	private ConferenceEventListener conferenceEventListener;
+	private static final int NTHREADS = 4;
+	private static final Executor exec = Executors.newFixedThreadPool(NTHREADS);
+	private BlockingQueue<IVoiceCommand> messages;
+	private volatile boolean send = false;
+	private Runnable sender;
 	
-	@Override
-    public void record(String room, String meetingid){
+	private ConferenceServiceProvider appDelegate;
+	
+	public void start() {
+		send = true;
+		sender = new Runnable() {
+			public void run() {
+				while (send) {
+					IVoiceCommand msg;
+					try {
+						msg = messages.take();
+						sendMessageServer(msg);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		};
+		exec.execute(sender);
+	}
+	
+	public void stop() {
+		send = false;
+	}
+	
+	public void sendMessage(IVoiceCommand message) {
+		messages.offer(message);
+	}
+	
+	private void sendMessageServer(IVoiceCommand msg) {
+		if (msg instanceof GetVoiceUsers) {
+			populateRoom((GetVoiceUsers) msg);
+		} else if (msg instanceof EjectVoiceUser) {
+			eject((EjectVoiceUser)msg);
+		} else if (msg instanceof MuteVoiceUser) {
+			
+		}
+	}
+	
+	private void record(String room, String meetingid){
     	appDelegate.record(room,meetingid);
     }
 
-	@Override
-    public void broadcast(String room, String meetingid){
+	private void broadcast(String room, String meetingid){
     	appDelegate.broadcast(room,meetingid);
     }
 	
-	@Override
-	public void eject(String room, Integer participant) {
-		appDelegate.eject(room, participant);
+	private void eject(EjectVoiceUser msg) {
+		//appDelegate.eject(room, participant);
 	}
 
-	@Override
-	public void mute(String room, Integer participant, Boolean mute) {
+	private void mute(String room, Integer participant, Boolean mute) {
 		appDelegate.mute(room, participant, mute);
 	}
 
-	@Override
-	public void populateRoom(String room) {
-		appDelegate.populateRoom(room);
-	}
-
-	@Override
-	public void shutdown() {
-		if ((connection != null) ) {
-			Client c = connection.getESLClient();
-			if((c != null ) && c.canSend()) {
-				log.info("Logging off fom [" + connection.toString() + "]");
-				connection.disconnect();
-            }
-		}
-		appDelegate.shutdown();
-	}
-
-	@Override
-	public boolean startup() {
-		if (connection == null) {
-			log.error("Cannot start application as ESL Client has not been set.");
-			return false;
-		}
-
-		if (connect()) {
-            return appDelegate.startup();
-		} else {
-			return false;
-		}
-	}
-	
-	private boolean connect() {
-		log.info("Logging in as [" + connection.getPassword() + "] to [" + connection.getHostname() + ":" + connection.getPort() + "]");
-		try {
-			connection.connect();
-			return true;
-        } catch ( InboundConnectionFailure e ) {
-			log.error( "Connect to FreeSwitch ESL socket failed", e );
-		} 
-		return true;
-	}
-	
-	public void setManagerConnection(ManagerConnection c) {
-		connection = c;
+	private void populateRoom(GetVoiceUsers msg) {
+		//appDelegate.populateRoom(room);
 	}
 
 	public void setFreeswitchApplication(FreeswitchApplication f) {
@@ -106,9 +101,4 @@ public class FreeswitchServiceProvider implements ConferenceServiceProvider {
 		
     }
 
-	@Override
-	public void setConferenceEventListener(ConferenceEventListener l) {
-		conferenceEventListener = l;		
-		appDelegate.setConferenceEventListener(conferenceEventListener);
-	}
 }
